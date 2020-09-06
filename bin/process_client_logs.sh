7307bf5b05eb2682d815b278b56e41aed87ac8c2
JacobJenness <jenne077@morris.umn.edu>
2020-09-05 21:58:21 -0500

    Finish process_client_logs.sh
    This commit finalizes process_client_logs.sh. It should now be
    in working order and it passes its bats tests.

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -1,19 +1,19 @@
-#!/bin/usr/env bash
+#!/usr/bin/env bash
 
 # Capture the directory to be worked in and the current directory
 # Change directory to the given directory from the command line
-directory=$1
+client_dir=$1
 here=$(pwd)
-cd "$directory" || exit
+cd "$client_dir" || exit
 
 # Create failed_login_data.txt
 touch failed_login_data.txt
 
 # Takes in all log files and then finds the info for invalid users and puts it in failed_login_data.txt
-cat ./*.* | awk -F"[ :]" '/Failed password for invalid user/ {print $1,$2,$3,$14,$16}' > failed_login_data.txt
+cat ./var/log/* | awk -F"[ :]+" '/Failed password for invalid user/ {print $1,$2,$3,$13,$15}'>> failed_login_data.txt
 
 # Takes in all log files and then finds the info for vaild users and puts it in failed_login_data.txt
-cat ./*.* | awk -F"[ :]" '/Failed password for/ {print $1,$2,$3,$12,$14}' > failed_login_data.txt
+cat ./var/log/* | awk -F"[ :]+" '/Failed password for/ && !/invalid/ {print $1,$2,$3,$11,$13}' >> failed_login_data.txt
 
 # Change back to the original directory
 cd "$here" || exit
