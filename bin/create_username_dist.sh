15f3374399848598a3e2386cae4e16814d808994
JacobJenness <jenne077@morris.umn.edu>
2020-09-06 15:59:14 -0500

    Finish create_username_dist.sh
    This commit adds a working version of create_username_dist.sh
    This commit also fixes a minor error in wrap_contents.sh

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -1,11 +1,38 @@
-#!usr/bin/env bash
+#!/usr/bin/env bash
 
-#move to directory
+#Capture the header directory
+#header=$(./html_components/username_dist_header.html)
+
+#Create a base name for the header
+#base=$(basename "$header" _header.html)
+
+#Move to directory
 cd ./"$1" || exit
-#gather contents of failed_login_data.txt into
-cat /{}/failed_login_data.txt > tmp_data.txt
-#sort input with 'sort'
+
+#Create the username_dist.html file
+touch username_dist.html
+
+#Gather the contents of every failed_login_data.txt file and put it into tmp_data.txt
+find . -type f -name 'failed_login_data.txt' -exec cat {} + > tmp_data.txt
+
+#Sort input with sort
 sort tmp_data.txt
-#count how many times each username appears with command 'uniq'
-#'awk' command that converts output of 'uniq' into desired 'data.addRow'
-#dump into 'tmp_file.txt' and pass to 'wrap_contents.sh'
+
+#Takes in tmp_data.txt and then filters out everything except usernames and puts it in username_dist.txt
+cat ./tmp_data.txt | awk -F"[ :]+" '/.*/ {print $4}' > username_dist.html
+
+#Sort usernames
+sort username_dist.html > tmp_data.txt
+
+#Count how many times each username appears with command uniq and pipes it
+#into awk, where it prints data.addRow(['username', repetitions]) into username_dist.html
+uniq -c tmp_data.txt | awk '/.*/ {print "data.addRow([\x27"$2"\x27, "$1"]);"}' > username_dist.html
+
+#Change directory to the top level
+cd ..
+
+#Take the contents of tmp_data.txt and pass it through wrap_contents.sh into username_dist.html
+./bin/wrap_contents.sh ./data/username_dist.html ./html_components/username_dist ./data/tmp_data.txt
+
+#Write the final wrapped content to username_dist.html
+cat ./data/tmp_data.txt > ./data/username_dist.html
--- bin/wrap_contents.sh
+++ bin/wrap_contents.sh
@@ -1,3 +1,3 @@
-#!/user/bin/env bash
+#!/usr/bin/env bash
 
 cat "$2"_header.html "$1" "$2"_footer.html > "$3"
