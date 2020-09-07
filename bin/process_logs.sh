0ef4afe6287abc9b15bea95313c80e134e9ad1ee
oswoo003 <oswoo003@umn.edu>
2020-09-06 21:26:24 -0500

    moved and fixed process_logs.sh

--- bin/process_logs.sh
+++ bin/process_logs.sh
@@ -4,19 +4,16 @@ tmp_dir=$(mktemp -d)
 #Loop over tar files and extract contents
 for i in "$@"
 do
-base_file_name=$(awk '/(-w\)_secure.tgz/')
-tar -xzf ./log_files/"$i" -C "$tmp_dir"/"$base_file_name"
+base_file_name=$(awk 'match($0, /(-w\)_secure.tgz/, base) {print base[1]}')
+tar -xzf ../log_files/"$i" -C "$tmp_dir"/"$base_file_name"
 #Call 'process_client_logs.sh'
 ./bin/process_client_logs.sh ./"$tmp_dir"/"$base_file_name"
 done
-#Call 'create_username_dist.sh'
-./bin/create_username_dist.sh "$tmp_dir"
-#Call 'create_hours_dist.sh'
-./bin/create_hours_dist.sh "$tmp_dir"
-#Call 'create_country_dist.sh'
-./bin/create_country_dist.sh "$tmp_dir"
-#Calls 'assemble_report.sh'
-./bin/assemble_report.sh "$tmp_dir"
+
+create_username_dist.sh "$tmp_dir"
+create_hours_dist.sh "$tmp_dir"
+create_country_dist.sh "$tmp_dir"
+assemble_report.sh "$tmp_dir"
 #Move resulting 'failed_login_summary.html' to directory where process_logs.sh was called.
 mv failed_login_summary.html /"$here"
 
