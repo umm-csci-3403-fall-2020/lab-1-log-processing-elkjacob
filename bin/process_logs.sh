171c30906a7bce34917a1a81061bfe50ae01afad
oswoo003 <oswoo003@umn.edu>
2020-09-08 11:02:56 -0500

    working on process_logs.sh

--- bin/process_logs.sh
+++ bin/process_logs.sh
@@ -1,21 +1,22 @@
 #!/usr/bin/env bash
 here=$(pwd)
+DIR="$(cd "$(dirname "$0")" && pwd)"
 tmp_dir=$(mktemp -d)
 #Loop over tar files and extract contents
 for i in "$@"
 do
-base_file_name=$(awk 'match($0, /(-w\)_secure.tgz/, base) {print base[1]}')
-tar -xzf ../log_files/"$i" -C "$tmp_dir"/"$base_file_name"
+awk 'match($0, /(\w)_secure.tgz/, group) {base_file_name=group[1]}' "$i"
+tar -xzf "$i" -C "$tmp_dir"/"$base_file_name"
 #Call 'process_client_logs.sh'
-./bin/process_client_logs.sh ./"$tmp_dir"/"$base_file_name"
+"$DIR"/process_client_logs.sh "$tmp_dir"/"$base_file_name"
 done
 
-create_username_dist.sh "$tmp_dir"
-create_hours_dist.sh "$tmp_dir"
-create_country_dist.sh "$tmp_dir"
-assemble_report.sh "$tmp_dir"
+"$DIR"/create_username_dist.sh "$tmp_dir"
+"$DIR"/create_hours_dist.sh "$tmp_dir"
+"$DIR"/create_country_dist.sh "$tmp_dir"
+"$DIR"/assemble_report.sh "$tmp_dir"
 #Move resulting 'failed_login_summary.html' to directory where process_logs.sh was called.
 mv failed_login_summary.html /"$here"
 
 #cleanup, rm temporary directory
-rm "$tmp_dir"
+rmdir "$tmp_dir"
